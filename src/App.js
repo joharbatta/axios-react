162f246504f4f1c0062bee6b524c358b9c4026e4
joharbatta <joharbatta@gmail.com>
2020-01-06 02:43:22 +0530

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React ,{Component} from 'react';
 import "./App.css"
-
+import UserFrom from "./components/UserForm";
 class App extends Component {
-  render()
+
+  getUser=(e)=>
   {
+    e.preventDefault();
+    const user=e.target.elements.username.value;
+    console.log(user);
+  } 
+  render() {
   return (
     <div className="App">
       <header className="App-header">
       <h1 className="App-title">HTTP Calls in React</h1>
 
       </header>
+      <UserFrom getUser={this.getUser}/>
     </div>
   );
   }
